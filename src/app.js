29850a6a351d99042a0df4cf122c0f8be393a472
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-06-18 16:08:17 +0530

    add CORS functionality

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+// Basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// Cors configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/instagram", (req, res) => {
+  res.send("this is instagram page");
+});
+
+export default app;
