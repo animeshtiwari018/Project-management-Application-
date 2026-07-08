f7678d927469bc82dfdf8b4b0a477a46b764507a
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-07-08 22:50:21 +0530

    feat :- add Username in model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,28 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  avatar: {
+    type: {
+      url: String,
+      localPath: String,
+    },
+    default: {
+      url: "https://placehold.co/200*200",
+      localPath: "",
+    },
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+
+  },
+
+  
+
+});
 
 export const userSchema = mongoose.model("User", userSchema);
